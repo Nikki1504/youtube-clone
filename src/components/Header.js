e950be3952cd467e8244f1d09d378c32f7e06b09
Nikki1504 <thakurnikita157@gmail.com>
2025-12-05 23:03:27 +0530

    installed other packages, added icons in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Search } from "lucide-react";
+import { CircleUserRound } from "lucide-react";
+import { Menu } from "lucide-react";
 
 const Header = () => {
   return (
     <div className="p-2 m-1 grid grid-flow-col shadow-md">
       <div className="flex col-span-1 items-center gap-2">
-        <img
-          className="h-8 cursor-pointer"
-          alt="menu"
-          src="https://cdn.iconscout.com/icon/free/png-256/free-hamburger-menu-icon-download-in-svg-png-gif-file-formats--crispy-user-interface-pack-icons-462145.png?f=webp&w=256"
-        />
+        <div className="p-2 rounded-full hover:bg-neutral-200 cursor-pointer">
+          <Menu />
+        </div>
         <a href="/">
           <img
             className="h-5 mx-2"
@@ -18,6 +18,7 @@ const Header = () => {
           />
         </a>
       </div>
+
       <div className="col-span-10 px-10 flex justify-center items-center">
         <div className="relative w-1/2 flex">
           <input
@@ -25,20 +26,16 @@ const Header = () => {
             type="text"
             placeholder="Search"
           />
-          <button className="border border-gray-400 rounded-r-full px-5 py-2 bg-gray-200">
+          <button className="border border-neutral-300 rounded-r-full px-5 py-2 bg-neutral-100">
             <Search />
           </button>
         </div>
       </div>
 
       <div className="col-span-1 flex justify-end">
-        <button className="flex items-center gap-2 border border-gray-200 rounded-full px-4 py-1 hover:bg-gray-100 transition">
-          <img
-            className="h-6"
-            alt="user-logo"
-            src="https://w7.pngwing.com/pngs/178/595/png-transparent-user-profile-computer-icons-login-user-avatars-thumbnail.png"
-          />
-          <span className="text-sm font-semibold">Sign In</span>
+        <button className="flex items-center gap-1 border border-gray-200 rounded-full px-3 py-1 hover:bg-blue-100 hover:border-transparent transition">
+          <CircleUserRound className="text-blue-600" />
+          <span className="text-sm text-blue-600 font-semibold">Sign in</span>
         </button>
       </div>
     </div>
