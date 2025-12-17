edd6b62774d0f7a2fcdd47712c6c70d7b3098c16
Nikki1504 <thakurnikita157@gmail.com>
2025-12-17 15:21:58 +0530

    move API key to env variables

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { Provider } from "react-redux";
 import Body from "./components/Body";
 import Header from "./components/Header";
 import appStore from "./utils/Store/appStore";
+import { createBrowserRouter } from "react-router-dom";
+
+const appRouter = createBrowserRouter([{}])
 
 function App() {
   return (
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyCf6Q_zj8gCsCUOwXzpsPR__B8xzhdUqNw";
+const GOOGLE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=" +
